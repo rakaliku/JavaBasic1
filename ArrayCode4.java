f2c8d374ef526d47b48824b018805c11e316e311
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2019-01-06 21:15:36 +0530

    codes

---
+++ ArrayCode4.java
@@ -0,0 +1,20 @@
+package com.java.ArraysExamples;
+
+public class ArrayCode4 {
+
+	public static void main(String[] args)
+	{
+		int[] a=new int[6];
+		a[0]=56;
+		a[1]=25;
+		a[2]=14;
+		a[3]=98;
+		a[4]=23;
+		a[5]=91;
+		for (int i=0;i<6;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
